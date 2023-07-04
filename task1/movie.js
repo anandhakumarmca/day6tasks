fa33c551c8ce84fdc42222f2e3a9fbdddf69fd70
Anandh Kumar <accetanandhmca@gmail.com>
2023-07-04 15:01:34 +0530

    Update movie.js

--- task1/movie.js
+++ task1/movie.js
@@ -1,5 +1,5 @@
 class Movie{// Create a Class as Movie
-    constructor(title,studio,rating){ //*a)create constructor for class Movie with title, studio and rating as aruguments and sets the respective class properties
+    constructor(title, studio, rating){ //*a)create a constructor for class Movie with title, studio, and rating as arguments and sets the respective class properties
         this.title = title;
         this.studio = studio;
         this.rating = rating || "PG";//*b) set the class property rating to "PG" as default when no rating is provided
@@ -14,11 +14,11 @@ const movies = [
   new Movie("Ponniyin Selvan", "LYCA", "PG"),
   new Movie("The Great Indian Kitchen", "KKR", "PG"),
   new Movie("Por Thozhil", "APPLAUSE", "PG-13"),
-  new Movie("Viduthalai ", "GRASSROOT")// did't set rating it will consider as "PG" which already mentioned in class
-];//*c)create movieArray for getPG method
+  new Movie("Viduthalai ", "GRASSROOT")// didn't set rating it will consider as "PG" which already mentioned in class
+];//*c)create movieArray for the get method
 
 const pgMovies = Movie.getPG(movies);
-console.log(pgMovies);//display the movie which has rating as "PG"
+console.log(pgMovies);//display the movie which has a rating as "PG"
 
 const englishMovie = new Movie("Casino Royale", "Eon Productions", "PG13");//*d)creates an instance of the class Movie 
 console.log(englishMovie);//display the englishMovie values
