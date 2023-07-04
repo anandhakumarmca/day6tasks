1eee5fa63a9f8980576aafe1bd6cdcddddc6417d
Anandh Kumar <accetanandhmca@gmail.com>
2023-07-04 21:55:51 +0530

    Update movie.js

--- task1/movie.js
+++ task1/movie.js
@@ -2,11 +2,19 @@ class Movie{// Create a Class as Movie
     constructor(title, studio, rating){ //*a)create a constructor for class Movie with title, studio, and rating as arguments and sets the respective class properties
         this.title = title;
         this.studio = studio;
-        this.rating = rating || "PG";//*b) set the class property rating to "PG" as default when no rating is provided
+        this._rating = rating;
     }
+    
+    set rating(newRating) {//*b) set the class property rating to "PG" as default when no rating is provided
+    if (newRating) {
+      this._rating = newRating;
+    } else {
+      this._rating = "PG";
+    }
+  }
 
     static getPG(movieArray){
-        return movieArray.filter(movie => movie.rating === "PG");
+        return movieArray.filter(movie => movie._rating === "PG");
     }//*c)getPG method returns a new array of only those movies in the input array with a rating of "PG"
 }
 
@@ -17,8 +25,13 @@ const movies = [
   new Movie("Viduthalai ", "GRASSROOT")// didn't set rating it will consider as "PG" which already mentioned in class
 ];//*c)create movieArray for the get method
 
-const pgMovies = Movie.getPG(movies);
-console.log(pgMovies);//display the movie which has a rating as "PG"
 
-const englishMovie = new Movie("Casino Royale", "Eon Productions", "PG13");//*d)creates an instance of the class Movie 
-console.log(englishMovie);//display the englishMovie values
+const spiderMan = new Movie("Spider Man", "Sony Pictures");
+spiderMan.rating = "PG"; // Setting the rating using the setter
+console.log("Displaying SpiderMan Values \n",spiderMan); // display the spiderMan values
+
+const pgMovies = Movie.getPG(movies);
+console.log("\nDisplaying All Movies Which has a rating as PG \n",pgMovies);//display the movie which has a rating as "PG"
+
+const casinoRoyale = new Movie("Casino Royale", "Eon Productions", "PG13");//*d)creates an instance of the class Movie 
+console.log("\nDisplaying casinoRoyale Values \n",casinoRoyale);//display the casinoRoyale values
